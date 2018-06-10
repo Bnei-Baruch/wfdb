abc347005af1eba0d9dfdd37f5b7c7037263c32a
Amnon Israely <amnonbb@gmail.com>
2018-06-10 16:47:27 +0300

    Change property order

--- model_archive.go
+++ model_archive.go
@@ -13,14 +13,14 @@ type archive struct {
 	FileName string		`json:"file_name"`
 	Language string		`json:"language"`
 	Extension string	`json:"extension"`
-	SendID  string  	`json:"send_id"`
-	Source  string  	`json:"source"`
 	Size int64			`json:"size"`
 	Sha1 string			`json:"sha1"`
+	SendID  string  	`json:"send_id"`
+	Source  string  	`json:"source"`
 }
 
 func findArFiles(db *sql.DB, key string, value string) ([]archive, error) {
-	sqlStatement := `SELECT id, archive_id, date, file_name, language, extension, source, send_id, size, sha1 FROM archive WHERE `+key+` LIKE '%`+value+`' ORDER BY id`
+	sqlStatement := `SELECT id, archive_id, date, file_name, language, extension, size, sha1, send_id, source FROM archive WHERE `+key+` LIKE '%`+value+`' ORDER BY id`
 	rows, err := db.Query(sqlStatement)
 
 	if err != nil {
@@ -33,7 +33,7 @@ func findArFiles(db *sql.DB, key string, value string) ([]archive, error) {
 
 	for rows.Next() {
 		var a archive
-		if err := rows.Scan(&a.ID, &a.ArchiveID, &a.Date, &a.FileName, &a.Language, &a.Extension, &a.Source, &a.SendID, &a.Size, &a.Sha1); err != nil {
+		if err := rows.Scan(&a.ID, &a.ArchiveID, &a.Date, &a.FileName, &a.Language, &a.Extension, &a.Size, &a.Sha1, &a.SendID, &a.Source); err != nil {
 			return nil, err
 		}
 		arfiles = append(arfiles, a)
@@ -44,7 +44,7 @@ func findArFiles(db *sql.DB, key string, value string) ([]archive, error) {
 
 func getArFiles(db *sql.DB, start, count int) ([]archive, error) {
 	rows, err := db.Query(
-		"SELECT id, archive_id, date, file_name, language, extension, source, send_id, size, sha1 FROM archive ORDER BY id DESC LIMIT $1 OFFSET $2",
+		"SELECT id, archive_id, date, file_name, language, extension, size, sha1, send_id, source FROM archive ORDER BY id DESC LIMIT $1 OFFSET $2",
 		count, start)
 
 	if err != nil {
@@ -57,7 +57,7 @@ func getArFiles(db *sql.DB, start, count int) ([]archive, error) {
 
 	for rows.Next() {
 		var a archive
-		if err := rows.Scan(&a.ID, &a.ArchiveID, &a.Date, &a.FileName, &a.Language, &a.Extension, &a.Size, &a.Sha1); err != nil {
+		if err := rows.Scan(&a.ID, &a.ArchiveID, &a.Date, &a.FileName, &a.Language, &a.Extension, &a.Size, &a.Sha1, &a.SendID, &a.Source); err != nil {
 			return nil, err
 		}
 		arfiles = append(arfiles, a)
@@ -68,15 +68,15 @@ func getArFiles(db *sql.DB, start, count int) ([]archive, error) {
 
 func (a *archive) getArFile(db *sql.DB) error {
 
-	return db.QueryRow("SELECT id, archive_id, date, file_name, language, extension, size, sha1 FROM archive WHERE archive_id = $1",
-		a.ArchiveID).Scan(&a.ID, &a.ArchiveID, &a.Date, &a.FileName, &a.Language, &a.Extension, &a.Size, &a.Sha1)
+	return db.QueryRow("SELECT id, archive_id, date, file_name, language, extension, size, sha1, send_id, source FROM archive WHERE archive_id = $1",
+		a.ArchiveID).Scan(&a.ID, &a.ArchiveID, &a.Date, &a.FileName, &a.Language, &a.Extension, &a.Size, &a.Sha1, &a.SendID, &a.Source)
 }
 
 func (a *archive) postArFile(db *sql.DB) error {
 
 	err := db.QueryRow(
-		"INSERT INTO archive(archive_id, date, file_name, language, extension, source, send_id, size, sha1) VALUES($1, $2, $3, $4, $5, $6, $7, $8, $9) ON CONFLICT (archive_id) DO UPDATE SET (archive_id, date, file_name, language, extension, source, send_id, size, sha1) = ($1, $2, $3, $4, $5, $6, $7, $8, $9) WHERE archive.archive_id = $1 RETURNING id",
-		a.ArchiveID, a.Date, a.FileName, a.Language, a.Extension, a.Source, a.SendID, a.Size, a.Sha1).Scan(&a.ID)
+		"INSERT INTO archive(archive_id, date, file_name, language, extension, size, sha1, send_id, source) VALUES($1, $2, $3, $4, $5, $6, $7, $8, $9) ON CONFLICT (archive_id) DO UPDATE SET (archive_id, date, file_name, language, extension, size, sha1, send_id, source) = ($1, $2, $3, $4, $5, $6, $7, $8, $9) WHERE archive.archive_id = $1 RETURNING id",
+		a.ArchiveID, a.Date, a.FileName, a.Language, a.Extension, a.Size, a.Sha1, a.SendID, a.Source).Scan(&a.ID)
 
 	if err != nil {
 		return err
@@ -88,8 +88,8 @@ func (a *archive) postArFile(db *sql.DB) error {
 func (a *archive) updateArFile(db *sql.DB) error {
 
 	_, err :=
-		db.Exec("UPDATE archive SET (archive_id, date, file_name, language, extension, source, send_id, size, sha1) = ($1, $2, $3, $4, $5, $6, $7, $8, $9) WHERE aricha_id=$1",
-			a.ArchiveID, a.Date, a.FileName, a.Language, a.Extension, a.Source, a.SendID, a.Size, a.Sha1)
+		db.Exec("UPDATE archive SET (archive_id, date, file_name, language, extension, size, sha1, send_id, source) = ($1, $2, $3, $4, $5, $6, $7, $8, $9) WHERE aricha_id=$1",
+			a.ArchiveID, a.Date, a.FileName, a.Language, a.Extension, a.Size, a.Sha1, a.SendID, a.Source)
 
 	return err
 }
